1e82298e813228c48ae8d95239edd76d6401de7b
Delyan Angelov <delian66@gmail.com>
2025-11-17 13:57:14 +0200

    update the file sizes mentioned in the comments in main.v too

--- main.v
+++ main.v
@@ -3,7 +3,7 @@ fn C.MessageBoxA(hwnd voidptr, text &char, caption &char, utype u32) int
 C.MessageBoxA(0, c'Hello world.', c'Hi:', C.MB_OK)
 
 // An alternative, is to do the commented lines instead, although it will produce a
-// slightly larger executable (69KB, instead of 44KB), because it needs the `builtin`
+// slightly larger executable (45KB, instead of 20KB), because it needs the `builtin`
 // module for the .to_wide() methods, so you will need to edit the Makefile to
 // also NOT pass `-no-builtin`:
 // fn C.MessageBoxW(hwnd C.HWND, text &u16, caption &u16, utype u32) int
